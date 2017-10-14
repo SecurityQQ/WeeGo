b66d0e15104b98755c1ab45ce8e8e7b66c2f1156
Sergey Kiselev <kiselev.sg@gmail.com>
2017-10-14 22:04:11 +0300

    Update

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from telegram_bot_core import updater
 import logging
 
 if __name__ == '__main__':
-    # logging.basicConfig(
-    #     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    #     level=logging.ERROR)
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.ERROR)
     updater.start_polling()
     app.run(host='0.0.0.0', debug=True)
--- schema.sql
+++ schema.sql
@@ -1,6 +1,8 @@
 drop table if exists entries;
 create table entries (
   id integer primary key autoincrement,
+  chat_id integer,
+  message_id integer,
   title text not null,
   full_message text not null,
   author text not null,
@@ -9,6 +11,13 @@ create table entries (
 
 drop table if exists likes;
 create table likes (
+  id integer not null,
+  person text not null,
+  person_name text not null
+);
+
+drop table if exists dislikes;
+create table dislikes (
   id integer not null,
   person text not null,
   person_name text not null
