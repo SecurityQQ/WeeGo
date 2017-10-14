89ba05e717cd590f228b58aaff7f5118389de8c7
Sergey Kiselev <kiselev.sg@gmail.com>
2017-10-14 16:06:48 +0300

    Working version!

--- database.py
+++ database.py
@@ -1,22 +1,25 @@
 import sqlite3
 import os
+import threading
 
-DATABASE = 'flaskr.db'
+DATABASE = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'flaskr.db')
 
 
 def connect_db():
-    rv = sqlite3.connect(DATABASE, check_same_thread=False)
+    print(DATABASE)
+    rv = sqlite3.connect(DATABASE)
     rv.row_factory = sqlite3.Row
     return rv
 
 
-sqlite_db = None
+sqlite_dbs = {}
 
 def get_db():
-    global sqlite_db
-    if sqlite_db is None:
-        sqlite_db = connect_db()
-    return sqlite_db
+    thread_id = threading.current_thread().ident
+    global sqlite_dbs
+    if sqlite_dbs.get(thread_id, None) is None:
+        sqlite_dbs[thread_id] = connect_db()
+    return sqlite_dbs[thread_id]
 
 
 def init_db():
@@ -27,17 +30,19 @@ def init_db():
 
 
 def close_db(error):
-    global sqlite_db
-    if sqlite_db is not None:
-        sqlite_db.close()
-        sqlite_db = None
+    thread_id = threading.current_thread().ident
+    global sqlite_dbs
+    if sqlite_dbs.get(thread_id, None) is not None:
+        sqlite_dbs[thread_id].close()
+        sqlite_dbs[thread_id] = None
 
 
 def add_new_activity(title, author):
     db = get_db()
-    db.execute('insert into entries (title, author) values (?, ?)', (title, author))
+    cursor = db.cursor()
+    cursor.execute('insert into entries (title, author) values (?, ?)', (title, author))
     db.commit()
-    return db.lastrowid
+    return cursor.lastrowid
 
 
 def get_activities():
@@ -49,14 +54,13 @@ def get_activities():
 
 def get_likes(activity_id):
     db = get_db()
-    cur = db.execute('select distinct person from likes where id == (?)', (int(activity_id), ))
+    cur = db.execute('select distinct person from likes where id = (?)', (int(activity_id), ))
     entries = cur.fetchall()
     return [dict(x) for x in entries]
 
 
 def add_like(activity_id, user_id):
     # TODO: check if like exists
-    print(322, activity_id, user_id)
     db = get_db()
     db.execute('insert into likes (id, person) values (?, ?)', (activity_id, user_id))
     db.commit()
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from server import app
 from telegram_bot_core import updater
 
+import logging
 
 if __name__ == '__main__':
+    # logging.basicConfig(
+    #     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    #     level=logging.ERROR)
     updater.start_polling()
     app.run(host='0.0.0.0', debug=True)
