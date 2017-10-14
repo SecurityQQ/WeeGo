d117e1ad050c8c4d04001c7f1eee792c2c502a2c
SecurityQQ <flesh.game@mail.ru>
2017-10-14 12:04:29 +0200

    seperted files

--- main.py
+++ main.py
@@ -1,66 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Go buhnem!'
+from server import app
+from telegram_bot_core import updater
 
 
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, BaseFilter
-#
-#
-def start(bot, update):
-    update.message.reply_text('Hello World!')
-
-
-def hello(bot, update):
-    update.message.reply_text(
-        'Hello {}'.format(update.message.from_user.first_name))
-
-import re
-
-
-def extract_place(text):
-    res = re.findall('го в (\w*)', text.lower())
-    if len(res) > 0:
-        return res[0]
-    else:
-        assert False
-
-def echo(bot, update):
-    print(update.message.text)
-    print('finding place')
-    place = extract_place(update.message.text)
-    print(place)
-    bot.send_message(chat_id=update.message.chat_id, text="ЧУВАКИ, {} РЕАЛЬНО ЗОВЕТ ВАС В {}".format(update.message.from_user.first_name, place) + " Охуенно, сходите, потусуетесь, погнали!")
-
-def is_triggger(text):
-    res = re.findall('го в ([a-zA-Z]*)', text.lower())
-    return len(res) > 0
-
-
-class PlaceFilter(BaseFilter):
-
-    def filter(self, message):
-        return bool(message.text and not message.text.startswith('/') and is_triggger(message.text))
-
-placeFilter = PlaceFilter()
-
-
-updater = Updater('434073103:AAFlPzUSW6ZBMEGPhdjk9W7n0wkdvPlg2eA')
-
-
-echo_handler = MessageHandler(placeFilter, echo)
-
-
-updater.dispatcher.add_handler(echo_handler)
-updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
-
-updater.start_polling()
-# updater.idle()
-
-
-
-
+if __name__ == '__main__':
+    updater.start_polling()
+    app.run(host='0.0.0.0', debug=True)
---
+++ telegram_bot_core.py
@@ -0,0 +1,51 @@
+
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, BaseFilter
+#
+#
+def start(bot, update):
+    update.message.reply_text('Hello World!')
+
+
+def hello(bot, update):
+    update.message.reply_text(
+        'Hello {}'.format(update.message.from_user.first_name))
+
+import re
+
+
+def extract_place(text):
+    res = re.findall('го в (\w*)', text.lower())
+    if len(res) > 0:
+        return res[0]
+    else:
+        assert False
+
+def echo(bot, update):
+    print(update.message.text)
+    print('finding place')
+    place = extract_place(update.message.text)
+    print(place)
+    bot.send_message(chat_id=update.message.chat_id, text="ЧУВАКИ, {} РЕАЛЬНО ЗОВЕТ ВАС В {}".format(update.message.from_user.first_name, place) + " Охуенно, сходите, потусуетесь, погнали!")
+
+def is_triggger(text):
+    res = re.findall('го в ([a-zA-Z]*)', text.lower())
+    return len(res) > 0
+
+
+class PlaceFilter(BaseFilter):
+
+    def filter(self, message):
+        return bool(message.text and not message.text.startswith('/') and is_triggger(message.text))
+
+placeFilter = PlaceFilter()
+
+
+updater = Updater('434073103:AAFlPzUSW6ZBMEGPhdjk9W7n0wkdvPlg2eA')
+
+
+echo_handler = MessageHandler(placeFilter, echo)
+
+updater.dispatcher.add_handler(echo_handler)
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
